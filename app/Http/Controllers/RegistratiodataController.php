9bbaa46f95f3eb603054568c5cafd13c4fe6a53f
harsh.patel <sojitraharsh6233@gmail.com>
2020-02-17 20:52:39 +0530

    First commit

---
+++ app/Http/Controllers/RegistratiodataController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistratiodataController extends Controller
+{
+    public function submit($Request)
+    {
+        print_r($Request);die;
+    }
+}
